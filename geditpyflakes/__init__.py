7cba3363ea629ea1902b528c45f9c514aa4e122b
Anna Warzecha <anna.warzecha@gmail.com>
2011-09-29 05:30:29 +0800

    Check if tag_start and tag_end are set after search

--- geditpyflakes/__init__.py
+++ geditpyflakes/__init__.py
@@ -69,8 +69,7 @@ class PyflakesPlugin(GObject.Object, Gedit.ViewActivatable):
             line_end = document.get_iter_at_line(line)
             line_end.forward_to_line_end()
             keyword = None
-            tag_start = line_start
-            tag_end = line_end
+            tag_start, tag_end = line_start, line_end
             if isinstance(problem, (messages.UnusedImport,
                                     messages.RedefinedWhileUnused,
                                     messages.ImportShadowedByLoopVar,
@@ -89,6 +88,9 @@ class PyflakesPlugin(GObject.Object, Gedit.ViewActivatable):
                 while offset.in_range(line_start, line_end):
                     tag_start, tag_end = offset.forward_search(keyword, 0,
                                                                line_end)
+                    if not tag_start or not tag_end:
+                        tag_start, tag_end = line_start, line_end
+                        break
                     if tag_start.starts_word() and tag_end.ends_word():
                         break
                     offset.forward_word_end()
